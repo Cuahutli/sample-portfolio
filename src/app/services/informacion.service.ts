3f4e1190c3f00128ea8459af08f07c4631ccd28a
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-08-14 12:33:43 -0600

    Servicio de Firebase listo, consumiendolo"

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -4,15 +4,30 @@ import { Http } from "@angular/http";
 @Injectable()
 export class InformacionService {
   info:any = {};
+  equipo:any[] = [];
   cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
 
   constructor( public http:Http) {
-      this.http.get("assets/data/info.pagina.json")
-               .subscribe( data =>{
-                  console.log(data.json());
-                  this.info = data.json();
-                  this.cargada = true;
-               })
+      this.carga_info();
+      this.carga_sobre_nosotros();
+   }
+
+   public carga_info(){
+     this.http.get("assets/data/info.pagina.json")
+              .subscribe( data =>{
+                 this.info = data.json();
+                 this.cargada = true;
+              });
+   }
+
+   public carga_sobre_nosotros(){
+     this.http.get("https://paginaweb-efa39.firebaseio.com/equipo.json")
+              .subscribe( data =>{
+                 console.log(data.json());
+                 this.equipo = data.json();
+                 this.cargada_sobre_nosotros = true;
+              })
    }
 
 }
