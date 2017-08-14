51f1dd502f0abbcda9a1d2930ea7669238497111
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-08-14 14:13:30 -0600

    agregando el cargando

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -3,19 +3,22 @@ import { Http } from '@angular/http';
 @Injectable()
 export class ProductosService {
   productos:any[] = [];
-  cargando_productos:boolean = false;
+  cargando_productos:boolean = true;
 
   constructor( private http:Http) {
       this.cargar_productos();
   }
 
   public cargar_productos(){
+      this.cargando_productos = true;
       this.http.get('https://paginaweb-efa39.firebaseio.com/productos_idx.json')
               .subscribe( res => {
                   console.log(res.json());
-                  this.productos = res.json();
-                  this.cargando_productos = true;
-                  
+                  setTimeout( () => {
+                    this.productos = res.json();
+                    this.cargando_productos = false;
+                  }, 500);
+
               });
   }
 
